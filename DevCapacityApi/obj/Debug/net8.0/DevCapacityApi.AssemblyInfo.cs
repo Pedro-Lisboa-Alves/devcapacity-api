6b56cd0f6d9597dd08da1da6b5636163c49e7fe2
Pedro Alves <pl.lisboa@gmail.com>
2025-09-29 16:54:00 +0100

    Update Dlls

--- DevCapacityApi/obj/Debug/net8.0/DevCapacityApi.AssemblyInfo.cs
+++ DevCapacityApi/obj/Debug/net8.0/DevCapacityApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DevCapacityApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e75de11d0ada480b26f093ef5b1a78f5270c10c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+106f13b2197f8d2e895711a81d4757b6d0f484c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("DevCapacityApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DevCapacityApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
