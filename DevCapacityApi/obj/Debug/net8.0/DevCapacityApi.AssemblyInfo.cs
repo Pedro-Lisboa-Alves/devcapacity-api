0b80c109c825a577e5f3b510b850384dfde1a652
Pedro Alves <pl.lisboa@gmail.com>
2025-10-22 15:26:40 +0100

    Sync

--- DevCapacityApi/obj/Debug/net8.0/DevCapacityApi.AssemblyInfo.cs
+++ DevCapacityApi/obj/Debug/net8.0/DevCapacityApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DevCapacityApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20aae546fdd3fafc9388e5572146eeb87a1f2c3f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89f8c5a1f6de0b26f1d9ebfaab8e8b12ed218544")]
 [assembly: System.Reflection.AssemblyProductAttribute("DevCapacityApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DevCapacityApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
