1252da96536833c6f7931b0da1378dffa403d51b
Pedro Alves <pl.lisboa@gmail.com>
2025-10-29 14:50:25 +0000

    Add docker configuration

--- DevCapacityApi/obj/Debug/net8.0/DevCapacityApi.AssemblyInfo.cs
+++ DevCapacityApi/obj/Debug/net8.0/DevCapacityApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DevCapacityApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd9ac2aa6c449546d631987d9b3ef34ebf72a6a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db1dc3c05147f75eb0496d7f58b6f39bc13676ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("DevCapacityApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DevCapacityApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
